74dcdb339fac484ecd195d02f1cf45b690e8e6ca
Kimgayoung98 <rkdud0520@naver.com>
2017-10-11 16:44:52 +0900

    update
    modify some code(exclude function refactoring)

--- Chrome_Visit_history.py
+++ Chrome_Visit_history.py
@@ -18,27 +18,27 @@ def getFiletime(dtms):
 		days, seconds = divmod(seconds, 86400)
 		return str(datetime.datetime(1601, 1, 1) + (datetime.timedelta(days, seconds, micros, hours =+ 9)))
 
-def print_url(int):
-	for num,row in enumerate(rows):
-		if(row[0] == int):
-			temp = row[1]
-			return temp
-
-def url_details(int):
-	for num,row in enumerate(rows):
-		if(row[0] == int):
+def match_url(value):
+	for row in (rows):
+		if(row[0] == value):
+			url = row[1]
+			return url
+			
+def url_details(value):
+	for row in (rows):
+		if(row[0] == value):
 			temp = row[2]
 			return temp
 
-def url_count(int):
-	for num,row in enumerate(rows):
-		if(row[0] == int):
+def url_count(value):
+	for row in (rows):
+		if(row[0] == value):
 			temp = row[3]
 			return temp
 
-def Last_Visit_time(int):
-	for num,row in enumerate(rows):
-		if(row[0] == int):
+def Last_Visit_time(value):
+	for row in (rows):
+		if(row[0] == value):
 			temp = row[5]
 			return str(getFiletime(temp))	
 
@@ -73,32 +73,26 @@ def getMac() :
 
     return nics
 
-# f1 = open('E:\chrome_url_info.txt', 'w+', encoding='utf8')
 time = datetime.datetime.now()
 curr_time = ("%s%s%s" % (time.year, time.month, time.day)) + '_' + time.strftime("%H%M%S")
-
 computer_N = socket.gethostname()
 
-f2 = open("C:\\Users\\Public\\Documents\\" + computer_N + '_'+ curr_time + '_ChromeHistory'+'.txt', 'w+', encoding='utf8')
-# f2 = open('E:\chrome_history_info.txt', 'w+', encoding='utf8')
+f = open("C:\\Users\\Public\\Documents\\" + computer_N + '_'+ curr_time + '_ChromeHistory'+'.txt', 'w+', encoding='utf8')
 
 data_path = os.path.expanduser('~') + "\\AppData\\Local\\Google\\Chrome\\User Data\\Default"
 files = os.listdir(data_path)
 history_db = os.path.join(data_path, 'history')
-#conn = sql.connect(r"C:\Users\gayou\AppData\Local\Google\Chrome\User Data\Default\History")
+
 conn = sql.connect(history_db)
 
 cur = conn.cursor()
 cur2 = conn.cursor()
-cur3 = conn.cursor()
 
 cur.execute("SELECT * FROM urls")
 cur2.execute("SELECT url, visit_time FROM visits")
-cur3.execute("SELECT url, id FROM urls")
 
 rows = cur.fetchall()
 rows2 = cur2.fetchall()
-rows3 = cur3.fetchall()
 
 nics = getMac()
 for nic in nics :
@@ -106,99 +100,48 @@ for nic in nics :
 	    MAC = v
 	    MAC2 = MAC.replace(':', '-')
 
-script = "http://mail.naver.com/login"
-parse = urlparse(script)[1]
-print(parse)
-
-
-
-#urls
-
-# print('------------------------------------URL_INFOMATION-------------------------------------')
-# for num,row in enumerate(rows):
-# 	print (socket.gethostname())
-# 	print ('URL list [' + str(num + 1) + ']' + '\n' + 'URL ' +  ': ' + str(row[1])
-# 		+ '\n'+'Details ' +  ': ' + str(row[2]) + '\n'+'Visit_Count ' +  ': ' + str(row[3])
-# 		+ '\n' + 'last_visit_time ' +  ': ' + getFiletime(row[5]).replace(" ", " : "))
-
-# 	host_name = socket.gethostname()
-# 	IP = str(get_IP())
-# 	user_name =  getpass.getuser()
-# 	url = (str(row[1]))
-# 	datails = (str(row[2]))
-# 	visit_count = (str(row[3]))
-# 	last_visit_time = (getFiletime(row[5]).replace(" ", " : "))
-
-
-	# f1.write("	")
-	# f1.write(' : ')
-	# f1.write(host_name)
-	# f1.write(' : ')
-	# f1.write(user_name)
-	# f1.write(' : ')
-	# f1.write(MAC2)
-	# f1.write(' : ')
-	# f1.write(IP)
-	# f1.write(' : ')
-	# f1.write(str((last_visit_time)))
-	# f1.write(' : ')
-	# f1.write(str((url)))
-	# f1.write(' : ')
-	# f1.write(str((datails)))
-	# f1.write(' : ')
-	# f1.write(str((visit_count)))
-	# f1.write('\n')
-			
-print ('-------------------------------------------------------------------------------------\n')
-print('\n')
-print('\n')
-print('\n')
-print ('---------------------------------------------------------------------------------------')
 print('-------------------------------------VISIT_HISTORY-------------------------------------')
 
 for num,row in enumerate(rows2):
-	print('Url_Id : ' + str(row[0]) + '\n' + 'URL: ' + print_url(row[0]) + '\n' + 'URI: ' + urlparse(print_url(row[0]))[1] + '\n' +'URL_Detail: ' + str(url_details(row[0])) 
-		+ '\n'+ 'Visit_Time : ' + getFiletime(row[1]).replace(" ", " : ") + '\n' + 'URL_Count: ' + str(url_count(row[0])))
-
-	print ('====================================================================================')
 
 	host_name = socket.gethostname()
 	IP = str(get_IP())
 	user_name =  getpass.getuser()
-	url = (print_url(row[0]))
+	url = (match_url(row[0]))
 	uri = urlparse(url)[1]
 	datails = (str(url_details(row[0])))
 	visit_time = (getFiletime(row[1]).replace(" ", ":::;"))
 	last_visit_time = (Last_Visit_time(row[0]).replace(" ", ":::;"))
 	count = str(url_count(row[0]))
-	
 
+	print('Url_Id : ' + str(row[0]) + '\n' + 'URL: ' + url + '\n' + 'URI: ' + uri + '\n' +'URL_Detail: ' + str(datails) 
+		+ '\n'+ 'Visit_Time : ' + visit_time + '\n' + 'Last_Visit_Time : ' + last_visit_time + '\n' + 'URL_Count: ' + count)
 
-	f2.write(host_name)
-	f2.write(':::;')
-	f2.write(user_name)
-	f2.write(':::;')
-	f2.write(IP)
-	f2.write(':::;')
-	f2.write(MAC2)
-	f2.write(':::;')
-	f2.write(str((visit_time)))
-	f2.write(':::;')
-	f2.write(last_visit_time)
-	f2.write(':::;')
-	f2.write(str((url)))
-	f2.write(':::;')
-	f2.write(str((uri)))
-	f2.write(':::;')
-	f2.write(str((datails)))
-	f2.write(':::;')
-	f2.write(str((count)))
-	f2.write('\n')	
+	print ('====================================================================================')
 
+	f.write(host_name)
+	f.write(':::;')
+	f.write(user_name)
+	f.write(':::;')
+	f.write(IP)
+	f.write(':::;')
+	f.write(MAC2)
+	f.write(':::;')
+	f.write(str((visit_time)))
+	f.write(':::;')
+	f.write(last_visit_time)
+	f.write(':::;')
+	f.write(str((uri)))
+	f.write(':::;')
+	f.write(str((url)))
+	f.write(':::;')
+	f.write(str((datails)))
+	f.write(':::;')
+	f.write(str((count)))
+	f.write('\n')	
 
 conn.close()
-# f1.close()
-f2.close()
+f.close()
 
 
 
